f55ede87c37209f3b497e91c2e99273270186c74
gympohnpimol <gympohnpimol@gmail.com>
2021-09-16 00:57:36 +0700

    djangodemo

---
+++ DjangoDemo/djangobasic/blogs/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def hello(request):
+    #Query data from model
+    data = Post.objects.all()
+    return render(request, 'index.html', {'posts':data})
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def createForm(request):
+    return render(request, 'form.html')
+
+def addBlog(request):
+    name = request.POST['name']
+    description = request.POST['description']
+    return render(request, 'result.html',
+        {
+            'name': name,
+            'description': description
+
+        }
+    )
+
